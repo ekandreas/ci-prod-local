5a1aa0acd77a8a1b1f5d059ca8446af83f4b61e0
Andreas Ek <andreas@flowcom.se>
2013-05-28 21:01:54 +0200

    Rewrite parameters and usage

--- fetchdb.py
+++ fetchdb.py
@@ -5,7 +5,7 @@ __author__ = 'Andreas Ek'
 from fabric.api import *
 
 # connect to sql, make a dump and get it
-def fetchdb( private_key_path, server_account, source_host, source_dbname, source_user, source_password, dest_name, dest_user, dest_password ):
+def fetchdb( private_key_path, server_account, source_host, source_dbname, source_user, source_password, dest_dbname, dest_user, dest_password ):
 
 	print 'Connect to the SQL and create a dump and transfer it to my computer'
 	print '=============================================================================='
@@ -13,14 +13,14 @@ def fetchdb( private_key_path, server_account, source_host, source_dbname, sourc
 	print 'set the connection'
 	env.key_filename = '{0}'.format(private_key_path)
 	env.use_ssh_config = True
-	env.user 	= '{0}'.format(account)
-	env.host_string = '{0}'.format(sqlhost)
+	env.user 	= '{0}'.format(source_account)
+	env.host_string = '{0}'.format(source_host)
 
 	print 'dump mysql to local tmp'
-	run( 'mkdir -p /tmp/getsqldump' )
-	run( 'mysqldump --user={0} --password={1} {2] > /tmp/getsqldump/{2}.sql'.format(sqluser, sqlpassword, sqlname) )
+	run( 'mkdir -p /tmp/fetchdb' )
+	run( 'mysqldump --user={0} --password={1} {2] > /tmp/fetchdb/{2}.sql'.format(source_user, source_password, source_dbname) )
 
 	print 'push mysql to local sql'
-	run( 'mysql -u {0} -p{1} {2} < /tmp/getsqldump/{3}.sql'.format(sql2user, sql2password, sql2name, sqlname) )
+	run( 'mysql -u {0} -p{1} {2} < /tmp/getsqldump/{3}.sql'.format(dest_user, dest_password, dest_dbname, source_dbname) )
 
 

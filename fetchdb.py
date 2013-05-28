89f59794c8dd550596b427f233236b2079d87f40
Andreas Ek <andreas@flowcom.se>
2013-05-28 22:32:31 +0200

    Cleanup

--- fetchdb.py
+++ fetchdb.py
@@ -8,49 +8,45 @@ def fetchdb():
 
     # expects environment variables as parameters from Jenkins
 
-    # private_key_path
-    # server_host
-    # server_account
-    # server_password
-		#
-    # source_dbname
-    # source_user
-    # source_password
-		#
-    # dest_dbname
-    # dest_user
-    # dest_password
+    private_key_path = env.private_key_path
+    server_host = env.server_host
+    server_account = env.server_account
+    server_password = env.server_password
 
-    local_env = env
+    source_dbname = env.source_dbname
+    source_user = env.source_user
+    source_password = env.source_password
 
-    print 'Connect to the SQL and create a dump and transfer it to my computer'
-    print '=============================================================================='
+    dest_dbname = env.dest_dbname
+    dest_user = env.dest_user
+    dest_password = dest_password
 
-    print 'set the connection'
-    env.key_filename = '{0}'.format(local_env.private_key_path)
+
+    # connect to the SQL and create a dump and transfer it to my computer
+
+    # set the connection
+    env.key_filename = private_key_path
     env.use_ssh_config = True
-    env.user = '{0}'.format(local_env.server_account)
+    env.user = server_account
 
-    if env.password:
-        env.password = '{0}'.format(local_env.server_password)
+    if server_password:
+        env.password = server_password
 
-    env.host_string = '{0}'.format(local_env.server_host)
+		# change host now!
+    env.host_string = server_host
 
-    print 'dump mysql to local tmp'
-    #sudo( 'mkdir -p /tmp/fetchdb' )
-    run('mysqldump --user={0} --password={1} {2} > /tmp/{2}.sql'.format(local_env.source_user,
-                                                                        local_env.source_password,
-                                                                        local_env.source_dbname))
+    # dump mysql to local tmp
+    run('mysqldump --user={0} --password={1} {2} > /tmp/{2}.sql'.format(source_user,
+                                                                        source_password,
+                                                                        source_dbname))
 
-    print 'get the mysql dump file to local tmp'
-    get('/tmp/{0}.sql'.format(local_env.source_dbname), '/tmp')
+    # get the mysql dump file to local tmp
+    get('/tmp/{0}.sql'.format(source_dbname), '/tmp')
 
-    print 'push mysql to local sql'
-    local('/Applications/MAMP/Library/bin/mysql -u {0} -p{1} {2} < /tmp/{3}.sql'.format(local_env.dest_user,
-                                                                                        local_env.dest_password,
-                                                                                        local_env.dest_dbname,
-                                                                                        local_env.source_dbname))
+    # push mysql to local sql
+    local('/Applications/MAMP/Library/bin/mysql -u {0} -p{1} {2} < /tmp/{3}.sql'.format(dest_user,
+                                                                                        dest_password,
+                                                                                        dest_dbname,
+                                                                                        source_dbname))
 
 
-def getparams():
-    print env

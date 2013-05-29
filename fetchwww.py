2c149b1b7ef7effa9aaa46779890b1fe3d5a03e9
Andreas Ek <andreas@flowcom.se>
2013-05-29 13:16:20 +0200

    fetchwww added

---
+++ fetchwww.py
@@ -0,0 +1,33 @@
+__author__ = 'Andreas Ek, Flowcom AB'
+
+# import the fabric lib into this script
+from fabric.api import *
+
+# to get all environment parameters from Jenkins, use os.environ:
+import os
+
+# connect to sql, make a dump and get it
+def fetchwww():
+    # expects environment variables as parameters from Jenkins
+
+    # connect to the SQL and create a dump and transfer it to my computer
+
+    # set the connection
+    env.key_filename = os.environ["private_key_path"]
+    env.use_ssh_config = True
+    env.user = os.environ["server_account"]
+
+    if os.environ["server_password"]:
+        env.password = os.environ["server_password"]
+
+    # change host now!
+    env.host_string = os.environ["server_host"]
+
+    # zip wp-content
+    run('zip -rq /tmp/fetchwww.zip {0}/wp-content/.'.format(os.environ["source_path"]))
+
+    # get wp-content
+    get('/tmp/fetchwww.zip', '/tmp')
+
+    # unzip wp-content
+    local('unzip /tmp/fetchwww.zip -qo -d {0}'.format(os.environ["destination_path"]))
